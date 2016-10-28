e4b89fbbaf67c4be90d817f71d6578b69e99d35a
David Messer <dmesser@me.com>
2016-10-28 15:47:45 -0400

    Added a few more sites

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -11,12 +11,11 @@
   'use strict';
 
   $(function () {
-    $('select.site-select').change(getProducts);
-    $('button[name="refresh"]').click(getProducts);
+
 
     function getProducts() {
-      $('.json-results').html('');
       var site = $('select[name=dropdown]').val();
+      $('.json-results').html('');
       $('.site-title').html('<h1>' + site + '</h1>');
       $.getJSON(site + '/products.json', function(data) {
         $.each(data, function(key, data) {
@@ -24,9 +23,9 @@
             $('.json-results').html('<h1>No Products Yet</h1>');
           } else {
           $.each(data, function (index, data) {
-            $('.json-results').append('<div class="product">' + '<h3>' + data.title + '</h3>' + '<img src="' + data.images[0].src + '" />' +  '<li>' + ' ' + data.tags + '</li>' + '<a target=_blank href="' + site + '/products/' + data.handle + '">PRODUCT PAGE</a><br /></div><br />');
+            $('.json-results').append('<div class="product">' + '<h3>' + data.title + '</h3>' + '<img src="' + data.images[0].src + '" />' +  '<li>' + ' ' + 'Tags/Keywords:' + ' ' + data.tags + '</li>' + '<a target=_blank href="' + site + '/products/' + data.handle + '">PRODUCT PAGE</a><br /></div><br />');
             $.each(data.variants, function(key, variant){
-              $('.json-results').append('<div class="variants">Add to Cart' + ' ' + '<a target=_blank href="' + site + '/cart/' + variant.id  + ':1">' + variant.title + '</a>' + ' ' + 'In stock:' + ' ' +  variant.available + ' ' + 'Price' + ' ' + variant.price +  '</div>');
+              $('.json-results').append('<div class="variants">Add to Cart' + ' ' + '<a target=_blank href="' + site + '/cart/' + variant.id  + ':1">' + variant.title + '</a>' + ' ' + 'In stock:' + ' ' + '<strong>' + variant.available + '</strong>' + ' ' + 'Price:' + ' ' + '<strong>' + variant.price + '</strong>' + '</div>');
             });
           });
           }
@@ -36,6 +35,9 @@
         $('.json-results').append('<li>An Error Occurred. Check site for details.</li>');
       });
     }
+
+    $('select.site-select').change(getProducts);
+    $('button[name="refresh"]').click(getProducts);
   });
 
 })(jQuery, window, document);
