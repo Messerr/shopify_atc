01ac7f5c88d58807b27c24f47b17ab99be46d523
David Messer <dmesser@me.com>
2016-10-28 13:07:59 -0400

    Add refresh button

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -25,6 +25,28 @@
         $('.json-results').append('<li>An Error Occurred. Check site for details.</li>');
       });
     });
+    $('button[name="refresh"]').on('click', function() {
+      $('.json-results').html('');
+      var site = $('select[name=dropdown]').val();
+      $('.site-title').html('<h1>' + site + '</h1>');
+      $.getJSON(site + '/products.json', function(data) {
+        $.each(data, function(key, data) {
+          if(data.length === 0) {
+            $('.json-results').html('<h1>No Products Yet</h1>');
+          } else {
+          $.each(data, function (index, data) {
+            $('.json-results').append('<div class="product">' + '<h3>' + data.title + '</h3>' + '<img src="' + data.images[0].src + '" />' +  '<li>' + ' ' + data.tags + '</li>' + '<a target=_blank href="' + site + '/products/' + data.handle + '">PRODUCT PAGE</a><br /></div><br />');
+            $.each(data.variants, function(key, variant){
+              $('.json-results').append('<div class="variants"><a target=_blank href="' + site + '/cart/' + variant.id  + ':1">' + variant.title + '</a>' + ' ' +  variant.available + ' ' + variant.price +  '</div>');
+            });
+          });
+          }
+        });
+      })
+      .fail(function() {
+        $('.json-results').append('<li>An Error Occurred. Check site for details.</li>');
+      });
+    });
   });
 
 })(jQuery, window, document);
