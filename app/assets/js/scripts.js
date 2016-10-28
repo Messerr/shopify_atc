e67723114c9c94ace6d65e3cccc19c7ab7ca5af2
David Messer <dmesser@me.com>
2016-10-28 14:05:43 -0400

    Bigger font sizes, more clear what each link does

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -11,7 +11,10 @@
   'use strict';
 
   $(function () {
-    $('select.site-select').change(function() {
+    $('select.site-select').change(getProducts);
+    $('button[name="refresh"]').click(getProducts);
+
+    function getProducts() {
       $('.json-results').html('');
       var site = $('select[name=dropdown]').val();
       $('.site-title').html('<h1>' + site + '</h1>');
@@ -23,7 +26,7 @@
           $.each(data, function (index, data) {
             $('.json-results').append('<div class="product">' + '<h3>' + data.title + '</h3>' + '<img src="' + data.images[0].src + '" />' +  '<li>' + ' ' + data.tags + '</li>' + '<a target=_blank href="' + site + '/products/' + data.handle + '">PRODUCT PAGE</a><br /></div><br />');
             $.each(data.variants, function(key, variant){
-              $('.json-results').append('<div class="variants"><a target=_blank href="' + site + '/cart/' + variant.id  + ':1">' + variant.title + '</a>' + ' ' +  variant.available + ' ' + variant.price +  '</div>');
+              $('.json-results').append('<div class="variants">Add to Cart' + ' ' + '<a target=_blank href="' + site + '/cart/' + variant.id  + ':1">' + variant.title + '</a>' + ' ' + 'In stock:' + ' ' +  variant.available + ' ' + 'Price' + ' ' + variant.price +  '</div>');
             });
           });
           }
@@ -32,29 +35,7 @@
       .fail(function() {
         $('.json-results').append('<li>An Error Occurred. Check site for details.</li>');
       });
-    });
-    $('button[name="refresh"]').on('click', function() {
-      $('.json-results').html('');
-      var site = $('select[name=dropdown]').val();
-      $('.site-title').html('<h1>' + site + '</h1>');
-      $.getJSON(site + '/products.json', function(data) {
-        $.each(data, function(key, data) {
-          if(data.length === 0) {
-            $('.json-results').html('<h1>No Products Yet</h1>');
-          } else {
-          $.each(data, function (index, data) {
-            $('.json-results').append('<div class="product">' + '<h3>' + data.title + '</h3>' + '<img src="' + data.images[0].src + '" />' +  '<li>' + ' ' + data.tags + '</li>' + '<a target=_blank href="' + site + '/products/' + data.handle + '">PRODUCT PAGE</a><br /></div><br />');
-            $.each(data.variants, function(key, variant){
-              $('.json-results').append('<div class="variants"><a target=_blank href="' + site + '/cart/' + variant.id  + ':1">' + variant.title + '</a>' + ' ' +  variant.available + ' ' + variant.price +  '</div>');
-            });
-          });
-          }
-        });
-      })
-      .fail(function() {
-        $('.json-results').append('<li>An Error Occurred. Check site for details.</li>');
-      });
-    });
+    }
   });
 
 })(jQuery, window, document);
